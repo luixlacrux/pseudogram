a7ddee3232d150af1eaff0c491353998f532a8de
luixlacrux <luixlacrux18@gmail.com>
2017-06-10 19:07:39 -0400

    [add] SigninForm component

---
+++ src/components/SigninForm.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import TextField from 'react-md/lib/TextFields'
+import Button from 'react-md/lib/Buttons/Button'
+
+const SigninForm = () => (
+  <form className="md-grid">
+    <TextField
+      id="signup-email"
+      label="Correo electronico"
+      type="email"
+      className="md-cell md-cell--12"
+      required
+    />
+    <TextField
+      id="signup-name"
+      label="Nombre completo"
+      className="md-cell md-cell--12"
+      required
+    />
+    <TextField
+      id="signup-username"
+      label="Nombre de usuario"
+      className="md-cell md-cell--12"
+      required
+    />
+    <TextField
+      id="signup-password"
+      label="Contrasena"
+      type="password"
+      className="md-cell md-cell--12"
+      required
+    />
+    <Button raised primary label="Registrarse" className="md-cell md-cell-12" />
+  </form>
+)
+
+export default SigninForm
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
+import Button from 'react-md/lib/Buttons/Button'
+
+import SigninForm from '../components/SigninForm'
 
 const Login = ({ user, location, dispatch }) => {
   const handleLogin = () => {
@@ -13,9 +16,32 @@ const Login = ({ user, location, dispatch }) => {
     return <Redirect to={from} />
   } else {
     return (
-      <div>
-        <button onClick={handleLogin}>Click to sign in</button>
-      </div>
+      <section className="Login md-grid">
+        <div className="Login-container md-cell md-cell--middle">
+          <h1 className="Login-title Title">Pseudogram</h1>
+          <h2 className="Login-subtitle">Regístrate para ver y compartir fotos.</h2>
+
+          <div className="social-btns md-cell">
+            <Button
+              raised
+              primary
+              label="Inicia session con Facebook"
+              iconClassName="fa fa-facebook-official"
+              onClick={handleLogin}
+            />
+            <Button
+              raised
+              secondary
+              label="Inicia session con Google"
+              iconClassName="fa fa-google"
+              onClick={handleLogin}
+            />
+          </div>
+
+          <div className="md-divider-border md-divider-border--below" />
+          <SigninForm />
+        </div>
+      </section>
     )
   }
 }
