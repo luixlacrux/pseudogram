d0d9a77684618cec2ef799d3300f6c1f1023e607
luixlacrux <luixlacrux18@gmail.com>
2017-06-13 21:58:35 -0400

    [update] replace App component with App container

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
+import { Route, Redirect, withRouter } from 'react-router-dom'
 
-import App from '../components/App'
+import App from '../containers/App'
 import SignIn from '../containers/SignIn'
 import SignUp from '../containers/SignUp'
 
@@ -27,15 +27,12 @@ const PublicRoute = ({ component: Component, authenticated, ...props }) => (
   )}/>
 )
 
-const NotFound = () => <h1>404 Not Found</h1>
-
 const Routes = ({ authenticated }) => (
-  <Switch>
-    <PrivateRoute path="/" exact component={App} authenticated={authenticated} />
-    <PublicRoute path="/signin"  component={SignIn} authenticated={authenticated} />
+  <div>
+    <PrivateRoute path="/" component={App} authenticated={authenticated} />
+    <PublicRoute path="/signin" component={SignIn} authenticated={authenticated} />
     <PublicRoute path="/signup" component={SignUp} authenticated={authenticated} />
-    <Route component={NotFound} />
-  </Switch>
+  </div>
 )
 
 function mapStateToProps (state = {}) {
